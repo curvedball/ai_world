7d599c686b6ea70582b5edcd1033e5e2c581800c
curvedball <zhangnuaa@163.com>
2019-01-06 12:09:41 +0800

    z

--- y_test/pytestcode/test.py
+++ y_test/pytestcode/test.py
@@ -1,13 +1,59 @@
 
-from numpy import *
+import numpy as np
+import pandas as pd
 
-# 一行三列
-v1 = array([[1, 1, 1]])
-print(v1)
+'''
+print('Hello world2!')
 
-# 两行三列
-v2 = array([[1, 1, 2], [1, 1, 0]])
-print(v2)
+a = np.random.randn(5, 1)
+print (a)
+print(a.T)
+print(a.shape)
+
+b = a.T
+print(b)
+'''
 
 
+
+
+'''
+#About 3ms
+print('Hello world3!')
+
+import time
+
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+tic = time.time()
+c = np.dot(a, b)
+toc = time.time()
+
+#print('time consumed: %s ms.\n' % ((toc - tic)*1000))
+print('time consumed: ' + str((toc - tic)*1000) + ' ms.')
+'''
+
+
+'''
+#about 300ms
+import time
+
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+c = 0
+
+tic = time.time()
+for i in range(1000000):
+    c = c + a[i] * b[i]
+toc = time.time()
+print('time consumed: ' + str((toc - tic)*1000) + ' ms.')
+'''
+
+
+#a = np.random.rand(3, 4)
+#print(a)
+
+a = np.random.randn(3, 5)
+print(a)
 
