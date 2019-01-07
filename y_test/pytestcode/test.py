2260174603a52c7a7f120277391afe6e98cdca36
curvedball <zhangnuaa@163.com>
2019-01-07 11:20:19 +0800

    z

--- y_test/pytestcode/test.py
+++ y_test/pytestcode/test.py
@@ -1,4 +1,5 @@
 
+'''
 from numpy import *
 
 # 一行三列
@@ -8,6 +9,18 @@ print(v1)
 # 两行三列
 v2 = array([[1, 1, 2], [1, 1, 0]])
 print(v2)
+'''
+
+
+'''
+from numpy import *
+print('hello')
+'''
+
+
+import tensorflow as tf
+print(tf.__version__)
+
 
 
 
